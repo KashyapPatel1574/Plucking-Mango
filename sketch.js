f266a7d7930ff0877800588dacc3c7c59d765223
KashyapPatel1574 <74296198+KashyapPatel1574@users.noreply.github.com>
2020-12-23 12:42:07 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,155 +3,84 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-const Render = Matter.Render;
 const Constraint = Matter.Constraint;
-var bobObject1,bobObject2,bobObject3, bobObject4,bobObject5, roofObject
-var rope1,rope2,rope3, rope4,rope5;
-var world;
+var boy,ground,rock,tree,m1,m2,m3,m4,m5,m6,m7,chain;
 
+function preload()
+{
+	boy = loadImage("boy.png");
+	tree = loadImage("tree.png");
+	backgroundImg = loadImage("bg.png");
+}
 
 function setup() {
-	createCanvas(1600, 700);
-	rectMode(CENTER);
+	createCanvas(1280, 400);
 
 
 	engine = Engine.create();
 	world = engine.world;
 
-	roofObject=new roof(width/2,height/4,width/7,20);
-
-	bobDiameter=40;
-
-	startBobPositionX=width/2;
-	startBobPositionY=height/4+500;
-	bobObject1=new bob(startBobPositionX-bobDiameter*2,startBobPositionY,bobDiameter);
-	bobObject2=new bob(startBobPositionX-bobDiameter,startBobPositionY,bobDiameter);
-	bobObject3=new bob(startBobPositionX,startBobPositionY,bobDiameter);
-	bobObject4=new bob(startBobPositionX+bobDiameter,startBobPositionY,bobDiameter);
-	bobObject5=new bob(startBobPositionX+bobDiameter*2,startBobPositionY,bobDiameter);
+	//Create the Bodies Here.
+	rock = new stone(320,225);
+	chain = new SlingShot(rock.body,{x:320 , y:225});
+	m1 = new mango(900,150,8);
+	m2 = new mango(950,120,10);
+	m3 = new mango(1000,100,7);
+	m4 = new mango(950,60,9);
+	m5 = new mango(1050,60,6);
+	m6 = new mango(1100,120,10);
+	ground = Bodies.rectangle(640,385,1290,20,{isStatic:true});
+	World.add(world,ground);
 	
-	
-	//Create a Ground
-	
-
-	var render = Render.create({
-	  element: document.body,
-	  engine: engine,
-	  options: {
-	    width: 1200,
-	    height: 700,
-	    wireframes: false
-	  }
-	});
-
-
-	rope1=new rope(bobObject1.body,roofObject.body,-bobDiameter*2, 0)
-
-	rope2=new rope(bobObject2.body,roofObject.body,-bobDiameter*1, 0)
-	rope3=new rope(bobObject3.body,roofObject.body,0, 0)
-	rope4=new rope(bobObject4.body,roofObject.body,bobDiameter*1, 0)
-	rope5=new rope(bobObject5.body,roofObject.body,bobDiameter*2, 0)
-
-	/*constraint1={
-		bodyA:bobObject1.body,
-		bodyB:roofObject.body,
-		pointB: {x:-bobDiameter*2, y:0}
-	}
-
-	constraint2={
-		bodyA:bobObject2.body,
-		bodyB:roofObject.body,		
-		pointB: {x:-bobDiameter, y:0}
-	}
-
-
-	constraint3={
-		bodyA:bobObject3.body,
-		bodyB:roofObject.body,		
-		pointB: {x:0, y:0}
-
-	}
-
-	constraint4={
-		bodyA:bobObject4.body,
-		bodyB:roofObject.body,		
-		pointB: {x:bobDiameter, y:0}	
-
-	}
-
-	constraint5={
-		bodyA:bobObject5.body,
-		bodyB:roofObject.body,		
-		pointB: {x:bobDiameter*2, y:0}
-	}
-
-	var pendulum1=Constraint.create(constraint1)
-	var pendulum2=Constraint.create(constraint2)
-	var pendulum3=Constraint.create(constraint3)
-	var pendulum4=Constraint.create(constraint4)
-	var pendulum5=Constraint.create(constraint5)
-
-	World.add(world, pendulum1);
-	World.add(world, pendulum2);
-	World.add(world, pendulum3);
-	World.add(world, pendulum4);
-	World.add(world, pendulum5);
-	*/
 	Engine.run(engine);
-	//Render.run(render);
-  
+	  
+}
+function mouseDragged(){
+    Matter.Body.setPosition(rock.body,{x:mouseX,y:mouseY});
+}
+function mouseReleased(){
+    chain.fly();
 }
 
 
 function draw() {
-  rectMode(CENTER);
-  background(230);
-  roofObject.display();
+	if (keyIsPressed === true) {
+		chain.attach();
+	  }
 
-  rope1.display()
-  rope2.display()
-  rope3.display()
-  rope4.display()
-  rope5.display()	
-  bobObject1.display();
-  bobObject2.display();
-  bobObject3.display();
-  bobObject4.display();
-  bobObject5.display();
- 
-  
-  
+    rectMode(CENTER);
+    background(backgroundImg);
+    push();
+    rect(width/2,400,width,20);
+	chain.display();
+    drawSprites();
+    console.log(rock);
+    imageMode(CENTER);
+    image(boy,400,300,250,300);
+	image(tree,1000,180,400,400);
+	rock.display();
+	m1.display();
+	m2.display();
+	m3.display();
+	m4.display();
+	m5.display();
+	m6.display();
+	collision(rock,m1);
+	collision(rock,m2);
+	collision(rock,m3);
+	collision(rock,m4);
+	collision(rock,m5);
+	collision(rock,m6);
+
+	strokeWeight(3);
+	stroke(0);
+	fill(255);
+    text('Let us Pluck the Mangoes !😋', 285, 22);
 	
-  
- 
-  
-  
- 
 }
-
-function keyPressed() {
-  	if (keyCode === UP_ARROW) {
-
-    	Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-50,y:-45});
-
-  	}
+function collision(a,b){
+	var d = dist(a.body.position.x,a.body.position.y,b.body.position.x,b.body.position.y)
+	if(d <= 50){
+		Body.setStatic(b.body,false);
+	}
 }
-
-
-function drawLine(constraint)
-{
-	bobBodyPosition=constraint.bodyA.position
-	roofBodyPosition=constraint.bodyB.position
-
-	roofBodyOffset=constraint.pointB;
-	
-	roofBodyX=roofBodyPosition.x+roofBodyOffset.x
-	roofBodyY=roofBodyPosition.y+roofBodyOffset.y
-	line(bobBodyPosition.x, bobBodyPosition.y, roofBodyX,roofBodyY);
-}
-
-
-
-
-
-
